0ade7215e6156930c6f8a352ad3d3c231fc86e8b
Alba <2023_Alba.Segura@iticbcn.cat>
2024-02-16 10:54:41 +0100

    millorant el utilmaze

--- Main.java
+++ Main.java
@@ -6,12 +6,16 @@ import java.io.IOException;
 import static java.lang.System.out; // To make shorter System.out.println
 
 public class Main {
-
     //? Variables de clase
-
+    static class Player{
+        int[] playerPosi;
+        int intents;
+    }
     public static void main(String[] args) throws IOException{
         Util.printHead();
-        UtilMaze.showMaze(args[0]);
+        Player player= new Player();
+        player.intents = 1;
+        UtilMaze.showMaze(args[0], player.intents);
         // laberint no resolt
         // mostra laberint
         UtilMaze.readMaze(args[0]);
--- UtilMaze.java
+++ UtilMaze.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 public class UtilMaze {
     
-    public static void showMaze(String path){
-        File file = new File(path);
-        if(!file.exists()){
+    public static void showMaze(String path, int intents){
+        File file = new File(path); //crea un file indicant el path
+        if(!file.exists()){ //mira si el file indicat existeix
             Util.println("El fitxer no existeix");
             return;
         }
@@ -14,20 +14,63 @@ public class UtilMaze {
         System.out.println("Laberint: "+ path);
         Util.println("Encara no resolt");
         System.out.println();
-        Util.println("Intents actuals");
+        Util.println("Intents actuals: "+intents);
         
     }
 
-    public static String[] readMaze(String path) throws IOException{
+    public static char[][] readMaze(String path) throws IOException{
         FileReader fR = new FileReader(path);
         BufferedReader reader = new BufferedReader(fR);
         String line = reader.readLine();
         String[] size = line.split("x");
-        for(int i = 0;i< size.length; i++){
-            System.out.println(size[i]);
+        char[][] maze = new char[Integer.parseInt(size[0])][Integer.parseInt(size[1])];
+        for(int i = 0; i< maze.length; i++){
+            line = reader.readLine();
+            for(int o =0;o<maze[i].length;o++){
+                char posi = line.charAt(o);
+                maze[i][o] = posi;
+            }
         }
+        printMaze( maze);
         reader.close();
-        return size;
+        return maze;
     }
+
+    public static void printMaze(char[][] maze){
+        for(int i = 0; i< maze.length; i++){            
+            for(int p =0;p<maze[i].length;p++){
+                if(maze[i][p]=='E'){
+                    System.out.print('→');
+                }
+                else if( maze[i][p]=='G'){
+                    System.out.print(' ');
+                }
+                else if(i==0 && p ==0){
+                    System.out.print('┌');
+                }
+                else if(i==0 && p==maze[i].length-1){
+                    System.out.print('┐');
+                }
+                else if(i==maze.length-1 && p ==0){
+                    System.out.print('└');
+                }
+                else if(i==maze.length-1  && p==maze[i].length-1){
+                    System.out.print('┘');
+                }
+                else if(i==0 || i==maze.length-1){
+                    System.out.print('─');
+                }
+                else if(p==0 || p==maze[i].length-1){
+                    System.out.print('│');
+                }
+                else{
+                    System.out.print('·');
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    
     
 }
